ff0d2d9fa0e6ca06636608e9de21fba5e3a51192
Surya-1519 <surya2005andfamily@gmail.com>
2025-07-27 16:06:53 +0530

    Model training and MLflow completed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 
 from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from src.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from src.entity.config_entity import TrainingPipelineConfig
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 from src.logger import logging
 from src.exception import CustomException
@@ -32,7 +33,12 @@ if __name__=="__main__":
         logging.info("Initialising data transformation....")
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Data transformation successful!")
-        print(data_transformation_artifact)
+        
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        logging.info("Creating model trainer.....")
+        model_trainer = ModelTrainer(data_transformation_artifact,model_trainer_config)
+        model_trainer_artifact = model_trainer.initialise_model_trainer()
+        logging.info("Model trainer completed and model is saved.")
         
     except Exception as e:
         raise CustomException(e,sys)
--- src/utils/main_utils/utils.py
+++ src/utils/main_utils/utils.py
@@ -5,6 +5,10 @@ import os,sys
 import yaml
 import pickle
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score,accuracy_score,f1_score,precision_score,recall_score
+import json
+from typing import List
 
 
 def read_yaml(file_path:str)->dict:
@@ -47,4 +51,58 @@ def save_pickle_file(file_path:str,pkl_file:object):
     except Exception as e:
         raise CustomException(e,sys)
 
+def read_numpy_array(file_path:str)->np.array:
+    try:
+        logging.info("Reading numpy array....")
+        with open(file_path,"rb") as f:
+            logging.info("Array read successfully!")
+            return np.load(f)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
 
+def read_pickle_file(file_path:str)->object:
+    try:
+        logging.info("Reading pickle file....")
+        with open(file_path,"rb") as f:
+            logging.info("Pickle file read successfully!")
+            return pickle.load(f)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def save_json(file_path:str,obj:dict):
+    try:
+        with open(file_path,"w") as f:
+            json.dump(obj,f,indent=4)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,params,models)->List:
+    try:
+        report = {}
+        models_report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            grid = GridSearchCV(model,param,cv=3)
+            grid.fit(X_train,y_train)
+
+            model.set_params(**grid.best_params_)
+
+            model.fit(X_train,y_train)
+            y_pred = model.predict(X_test)
+
+            model_r2_score = r2_score(y_true=y_test,y_pred=y_pred)
+            report[list(models.keys())[i]] = model_r2_score
+            sub_report = {}
+            sub_report["f1_score"] = f1_score(y_test,y_pred)
+            sub_report["precision_score"] = precision_score(y_test,y_pred)
+            sub_report["recall_score"] = recall_score(y_test,y_pred)
+            sub_report["accuracy"] = accuracy_score(y_test,y_pred)
+            models_report[list(models.keys())[i]] = sub_report
+            ans:List = []
+            ans.append(report)
+            ans.append(models_report)
+        return ans
+    except Exception as e:
+        raise CustomException(e,sys)
